5afbedae96fd0246f8a02c389429b2b9cedd8b83
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-16 14:47:20 -0300

    11.11. Criando estilos globais

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { light } from "../styles/theme";
+import GlobalStyles from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // return <Component {...pageProps} />
   return (
     <ThemeProvider theme={light}>
       <Component {...pageProps} />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }

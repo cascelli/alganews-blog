3ec850d5490f4de5d0082a69d946bcbe94b71de3
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-20 12:44:22 -0300

    11.12. Criando o Layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,49 @@
 //import Head from "next/head";
 import styled from "styled-components";
+import { transparentize } from "polished";
+import { HEADER_HEIGHT } from "../_constants";
+
+// export default function Header(props: any) {
+//   return (
+//     <>
+//       {/* <Head>
+//         <title>batata</title>
+//       </Head> */}
+//       <Title>oi eu sou um header - {props.name}</Title>
+//     </>
+//   );
+// }
+
+// const Title = styled.h1`
+//   // color: blue;
+//   // Substituido valor fixo pela propriedade theme.primaryBackground
+//   color: ${(p) => p.theme.primaryBackground};
+// `;
 
 export default function Header(props: any) {
   return (
-    <>
-      {/* <Head>
-        <title>batata</title>
-      </Head> */}
-      <Title>oi eu sou um header - {props.name}</Title>
-    </>
+    <Wrapper>
+      <Container>
+        <span>logo</span>
+        <span>navbar</span>
+      </Container>
+    </Wrapper>
   );
 }
 
-const Title = styled.h1`
-  // color: blue;
-  // Substituido valor fixo pela propriedade theme.primaryBackground
-  color: ${(p) => p.theme.primaryBackground};
+const Wrapper = styled.div`
+  background-color: ${(p) => p.theme.activeElementBackground};
+  color: ${(p) => p.theme.activeElementForeground};
+  box-shadow: 0 3px 10px ${(p) => transparentize(0.9, p.theme.pageForeground)};
+  width: 100%;
+  height: ${HEADER_HEIGHT}px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 848px;
+  margin: auto;
+  height: 100%;
 `;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,19 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { light } from "../styles/theme";
 import GlobalStyles from "../styles/globalStyles";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Content from "../components/Content";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // return <Component {...pageProps} />
   return (
     <ThemeProvider theme={light}>
-      <Component {...pageProps} />
+      <Header />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+      <Footer />
       <GlobalStyles />
     </ThemeProvider>
   );

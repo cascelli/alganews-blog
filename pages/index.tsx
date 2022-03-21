2364a4c6925e7b99817df860ad7c289304e43b9e
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-21 15:30:18 -0300

    11.24. Validando parâmetros

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import { Post, PostService } from "danielbonifacio-sdk";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import FeaturedPost from "../components/FeaturedPost";
+import { ServerResponse } from "http";
 
 interface HomeProps {
-  posts: Post.Paginated;
+  posts?: Post.Paginated;
 }
 
 export default function Home(props: HomeProps) {
@@ -23,13 +24,32 @@ export default function Home(props: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async (
-  context
-) => {
-  const { page } = context.query;
+function sendToHomePage(res: ServerResponse) {
+  res.statusCode = 302;
+  res.setHeader("Location", "/?page=1");
+
+  return { props: {} };
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
+  query,
+  res,
+}) => {
+  const { page: _page } = query;
+
+  const page = Number(_page);
+
+  if (isNaN(page) || page < 1) {
+    return sendToHomePage(res);
+  }
+
   const posts = await PostService.getAllPosts({
     page: Number(page) - 1,
   });
+
+  if (!posts.content?.length) {
+    return sendToHomePage(res);
+  }
 
   return {
     props: {
